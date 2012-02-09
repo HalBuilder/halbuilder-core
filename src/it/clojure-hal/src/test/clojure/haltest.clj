378670cac3300e00050d5f5e5729885969170809
Mark Derricutt <mark@talios.com>
2012-02-09 22:49:37 +1300

    Updated clojure it

--- src/it/clojure-hal/src/test/clojure/haltest.clj
+++ src/it/clojure-hal/src/test/clojure/haltest.clj
@@ -3,7 +3,7 @@
 
 (deftest haltest
     (let [resource-factory (com.theoryinpractise.halbuilder.ResourceFactory.)
-          resource (doto (.newHalResource resource-factory "/foo")
+          resource (doto (.newResource resource-factory "/foo")
                          (.withProperty "name" "Mark")
                          (.withLink "/home" "home"))]
          (println (.renderContent (.asRenderableResource resource) "application/hal+xml"))) )
