ed3e3e80430f4d04e8b23f71185e5e286a2a7b4c
Mark Derricutt <mark@talios.com>
2012-02-27 22:25:00 +1300

    Added validation/contract test

---
+++ src/test/java/com/theoryinpractise/halbuilder/ValidationTest.java
@@ -0,0 +1,59 @@
+package com.theoryinpractise.halbuilder;
+
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.theoryinpractise.halbuilder.spi.Contract;
+import com.theoryinpractise.halbuilder.spi.ReadableResource;
+import org.testng.annotations.Test;
+
+import javax.annotation.Nullable;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class ValidationTest {
+
+
+    ResourceFactory resourceFactory = new ResourceFactory();
+
+    ReadableResource resource = resourceFactory.newResource(
+            new InputStreamReader(ResourceReaderTest.class.getResourceAsStream("example.xml")));
+
+
+    public static interface Namable {
+        String getName();
+    }
+
+
+    @Test
+    public void testValidation() {
+
+        Contract noWhiteSpaceInName = new Contract() {
+            public boolean isSatisfiedBy(ReadableResource resource) {
+                return (((String) resource.get("name").or("")).matches("\\W*"));
+            }
+        };
+
+        Contract anyCharsInName = new Contract() {
+            public boolean isSatisfiedBy(ReadableResource resource) {
+                return (((String) resource.get("name").or("")).matches(".*"));
+            }
+        };
+
+        assertThat(resource.isSatisfiedBy(noWhiteSpaceInName)).isFalse();
+        assertThat(resource.isSatisfiedBy(anyCharsInName)).isTrue();
+
+        Optional<Integer> length = resource.ifSatisfiedBy(Namable.class, new Function<Namable, Integer>() {
+            public Integer apply(@Nullable Namable input) {
+                System.out.println(input.getName());
+                return input.getName().length();
+            }
+        });
+
+        assertThat(length.get()).isEqualTo(16);
+
+    }
+
+
+}
