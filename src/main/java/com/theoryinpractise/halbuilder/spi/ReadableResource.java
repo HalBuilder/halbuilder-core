6a8751eb66cb3b701003d31f1b6ad55982c23d25
Joost Cassee <joost@cassee.net>
2012-04-18 15:31:02 +0200

    Replace tabs with spaces

--- src/main/java/com/theoryinpractise/halbuilder/spi/ReadableResource.java
+++ src/main/java/com/theoryinpractise/halbuilder/spi/ReadableResource.java
@@ -95,22 +95,22 @@ public interface ReadableResource {
      */
     <T, V> Optional<V> ifSatisfiedBy(Class<T> anInterface, Function<T, V> function);
 
-	/**
-	 * Returns an optional proxy to the given interface mirroring the resource.
-	 *
-	 * @param anInterface An interface to mirror
-	 * @return A Guava Optional Resource Proxy
-	 */
-	<T> Optional<T> renderClass(Class<T> anInterface);
+    /**
+     * Returns an optional proxy to the given interface mirroring the resource.
+     *
+     * @param anInterface An interface to mirror
+     * @return A Guava Optional Resource Proxy
+     */
+    <T> Optional<T> renderClass(Class<T> anInterface);
 
-	/**
-	 * Returns the resource in the request content-type.
-	 *
-	 * application/hal+xml and application/hal+json are provided by default,
-	 * additional Renderers can be added to a ResourceFactory.
-	 *
-	 * @param contentType The content type requested
-	 * @return A String
-	 */
-	String renderContent(String contentType);
+    /**
+     * Returns the resource in the request content-type.
+     *
+     * application/hal+xml and application/hal+json are provided by default,
+     * additional Renderers can be added to a ResourceFactory.
+     *
+     * @param contentType The content type requested
+     * @return A String
+     */
+    String renderContent(String contentType);
 }
