c5bf9c07cfd018605ea449602f4794a50014ae9b
Mark Derricutt <mark@talios.com>
2012-08-07 20:51:07 +1200

    Updated integration test.

--- src/it/clojure-hal/src/test/clojure/haltest.clj
+++ src/it/clojure-hal/src/test/clojure/haltest.clj
@@ -3,7 +3,7 @@
 
 (deftest haltest
     (let [representation-factory (com.theoryinpractise.halbuilder.RepresentationFactory.)
-          representation (doto (.newResource representation-factory "/foo")
+          representation (doto (.newRepresentation representation-factory "/foo")
                          (.withProperty "name" "Mark")
                          (.withLink "home" "/home"))]
          (println (.renderContent representation "application/hal+xml"))) )
