0fa223e9162c05b68dc69d8580a185b53990b1d9
Mark Derricutt <mark@talios.com>
2013-06-13 21:16:55 +1200

    Remove old integration test.
    Moved to new halbuilder-examples repository on github.

--- src/it/clojure-hal/src/test/clojure/haltest.clj
+++
@@ -1,9 +0,0 @@
-(ns haltest
-  (:use [clojure.test]))
-
-(deftest haltest
-    (let [representation-factory (com.theoryinpractise.halbuilder.DefaultRepresentationFactory.)
-          representation (doto (.newRepresentation representation-factory "/foo")
-                         (.withProperty "name" "Mark")
-                         (.withLink "home" "/home"))]
-         (println (.toString representation "application/hal+xml"))) )
