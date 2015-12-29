1689a85dce322390dcd95a42e23f811925c84943
Mark Derricutt <mark@talios.com>
2015-12-29 18:20:15 +1300

    Switch Support to interface

--- src/main/java/com/theoryinpractise/halbuilder/impl/api/Support.java
+++ src/main/java/com/theoryinpractise/halbuilder/impl/api/Support.java
@@ -5,28 +5,29 @@ import com.google.common.base.Splitter;
 import javaslang.collection.HashSet;
 import javaslang.collection.Set;
 
-public class Support {
+public interface Support {
 
-  public static final Splitter WHITESPACE_SPLITTER = Splitter.onPattern("\\s").omitEmptyStrings();
-  public static final String REL = "rel";
-  public static final String SELF = "self";
-  public static final String LINK = "link";
-  public static final String HREF = "href";
-  public static final String LINKS = "_links";
-  public static final String EMBEDDED = "_embedded";
-  public static final String NAME = "name";
-  public static final String CURIES = "curies";
-  public static final String TITLE = "title";
-  public static final String HREFLANG = "hreflang";
-  public static final String TEMPLATED = "templated";
-  public static final Set<String> RESERVED_JSON_PROPERTIES = HashSet.of(EMBEDDED, LINKS);
+  Splitter WHITESPACE_SPLITTER = Splitter.onPattern("\\s").omitEmptyStrings();
+  String REL = "rel";
+  String SELF = "self";
+  String LINK = "link";
+  String HREF = "href";
+  String LINKS = "_links";
+  String EMBEDDED = "_embedded";
+  String NAME = "name";
+  String CURIES = "curies";
+  String TITLE = "title";
+  String HREFLANG = "hreflang";
+  String TEMPLATED = "templated";
+  Set<String> RESERVED_JSON_PROPERTIES = HashSet.of(EMBEDDED, LINKS);
 
-  public static final String PROFILE = "profile";
+  String PROFILE = "profile";
 
-  public static void checkRelType(String rel) {
+  static void checkRelType(String rel) {
     Preconditions.checkArgument(rel != null, "Provided rel should not be null.");
     Preconditions.checkArgument(!"".equals(rel) && !rel.contains(" "),
         "Provided rel value should be a single rel type, as "
         + "defined by http://tools.ietf.org/html/rfc5988");
   }
+
 }
